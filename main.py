69a33a6847637495f85016b711884d685d319482
Håkon Guttulsrud <guttulsrud@outlook.com>
2023-03-19 16:40:00 +0100

    Added uploading of results

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from model.train import train_network
 from utils.config import get_config
 from utils.logger import Logger
+from utils.storage.upload_results import upload_results
 
 if __name__ == '__main__':
     config = get_config()
     logger = Logger(config=config)
     train_network(config=config, logger=logger)
+    upload_results(experiment_name=config['experiment_name'])
